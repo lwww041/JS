8ba21acc4a775d15b762f018ee16f13daca48700
“lwww041” <13880554397@163.com>
2024-12-15 19:54:51 +0800

    更新1

---
+++ 练习.js
@@ -0,0 +1,59 @@
+const quizdata=[
+    {
+        question:"最终话live[命运之花]在动画制作中使用了多少聚光灯？",
+        a:"180",
+        b:"175",
+        c:"188",
+        d:"182",
+        answer:"a"
+    },
+    {
+        question:"以下哪首歌曲的贝斯使用了大量失真效果？",
+        a:"伤伤痛辛",
+        b:"溶于黑暗",
+        c:"空白与宣泄",
+        d:"虚伪之理",
+        answer:"c"
+    },
+    {
+        question:"第三集新川崎三人组吃的包子来自？",
+        a:"mmk家的猫做的",
+        b:"486在商店街中奖得的",
+        c:"mmk喝醉的时候花光了所有的钱买的",
+        d:"486在中华街打工的朋友送的",
+        answer:"d"
+    },
+    {
+        question:"反正这座城市早已到了结局（どうせ終わってる街だって）",
+        a:"爆裂绽放",
+        b:"皆无其名",
+        c:"熙攘都市",
+        d:"虚伪之理",
+        answer:"c"
+    },
+    {
+        question:"Nina曾经有过为了涨粉不择手段的想法,她提出的具体方案是？",
+        a:"直播听一千遍空之箱",
+        b:"上传 486的JK照片",
+        c:"开盒新钻石星尘主唱",
+        d:"扮演新川崎女子学院",
+        answer:"b"
+    },
+    {
+        question:"广播剧：想收获全场爆笑中，桃香最后的结局是",
+        a:"和仁菜一起成为了漫才大师，成功收获全场爆笑",
+        b:"和仁菜一起前往中东寻找石油王，最后迷失在沙漠中",
+        c:"成为了施工工地严厉的包工头，狠狠鞭笞刺团成员",
+        d:"在和安和昴的游戏决斗中燃尽而死",
+        answer:"c"
+    },
+    {
+        question:"TV 动画中 TOGENASHI TOGEARI mmk吉他的乐器型号是？",
+        a:"ESP Original Horizon-I Candy Apple Red",
+        b:"G&L Tribute ASAT Classic",
+        c:"Psychederhythm Psychomaster 2020",
+        d:"HISTORY HJM-Performance Lake Placid Blue",
+        answer:"c"
+    }
+]
+
